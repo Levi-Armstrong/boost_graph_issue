f200ac9ad6ba6253edf27070f552a131f1d55adb
Levi Armstrong <levi.armstrong@gmail.com>
2020-10-16 11:12:21 -0500

    Initial commit

---
+++ src/graph_test.cpp
@@ -0,0 +1,9 @@
+// Type your code here, or load an example.
+
+#include <boost_graph_issue/graph.h>
+
+int main(int ac, char** av)
+{
+    boost_graph_issue::SceneGraph sg;
+    sg.setName("Hello");
+}
